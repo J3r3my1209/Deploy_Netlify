af343d2e6356ab21595ad43d85bc7c3363c674df
J3r3my1209 <vivasmoreirajeremy@gmail.com>
2026-06-10 15:43:36 -0500

    /

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -12,4 +12,18 @@ exports.handler = async function(event, context) {
       fecha: new Date().toISOString()
     })
   };
+// netlify/functions/health.js
+
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ok: true,
+      message: "Backend funcionando desde Netlify Functions",
+      fecha: new Date().toISOString()
+    })
+  };
 };
