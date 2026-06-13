d0a92b978ac4ee097e73b8b7946e7aa503fa558f
J3r3my1209 <vivasmoreirajeremy@gmail.com>
2026-06-12 19:14:16 -0500

    /

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -9,16 +9,4 @@ exports.handler = async function() {
       docente: process.env.DOCENTE || "Sin docente"
     })
   };
-// netlify/functions/config.js
-
-exports.handler = async function() {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      appName: process.env.APP_NAME || "Sin nombre",
-      docente: process.env.DOCENTE || "Sin docente"
-    })
-  };
-};
 };
